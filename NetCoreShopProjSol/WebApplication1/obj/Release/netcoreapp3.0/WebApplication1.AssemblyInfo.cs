a2eb31960bcfff8017b1fb048595124c88a4f4b7
Артём Чернышов <inviolablejade@gmail.com>
2021-02-04 01:15:09 +0400

    Started work on a real-time wep app.

--- NetCoreShopProjSol/WebApplication1/obj/Release/netcoreapp3.0/WebApplication1.AssemblyInfo.cs
+++ NetCoreShopProjSol/WebApplication1/obj/Release/netcoreapp3.0/WebApplication1.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("01685df6-b3ad-40a8-ac9f-e241512cdaf9")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
